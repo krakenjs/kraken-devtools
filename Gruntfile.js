bc883b0ae65b61bd4d1bb8628802c689a1197302
Steve Stedman <sstedman@paypal.com>
2013-11-27 23:35:38 -0600

    Increased JSHint coverage.

--- Gruntfile.js
+++ Gruntfile.js
@@ -20,6 +20,12 @@
 module.exports = function (grunt) {
 
     grunt.initConfig({
+        jshint: {
+            files: ['Gruntfile.js', 'index.js', 'lib/**/*.js', 'test/**/*.js'],
+            options: {
+                jshintrc: '.jshintrc'
+            }
+        },
         mochaTest: {
             src: ['test/*.js'],
             options: {
@@ -32,8 +38,9 @@ module.exports = function (grunt) {
         }
     });
 
+    grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-mocha-test');
 
-    grunt.registerTask('test', ['mochaTest']);
+    grunt.registerTask('test', ['jshint', 'mochaTest']);
 
 };
